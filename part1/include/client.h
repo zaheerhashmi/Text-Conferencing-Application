98a7cb9a229effabda4cbae09bda01431c7c4950
harris222 <harriszheng@live.com>
2021-03-23 14:44:49 -0400

    Client sending and receiving all work fine. I'm not sure how to process the command menu and receive from the server at the same time yet, so I just used a lock, which should work but it's heavily dependent on the server so I'm not sure if it works well yet.

--- part1/include/client.h
+++ part1/include/client.h
@@ -13,21 +13,26 @@ enum state {
  *  The client number to be shared globally
  */
 extern char my_username[INPUT_LENGTH];
+extern char current_session[INPUT_LENGTH];
 extern struct IPInfo info;
+extern pthread_mutex_t mutex;
 
 /** -------------------------------------------
  *  Client Functions
  * -------------------------------------------- */
-void construct_packet_client(struct Message, packet_t type, char * finalPacket);
-sock_t login(int nargs, char ** args, enum state *);
-void logout(int nargs, char ** args, enum state *);
-void join_session(int nargs, char ** args, enum state *);
-void leave_session(int nargs, char ** args, enum state *);
-void create_session(int nargs, char ** args, enum state *);
-void list(int nargs, char ** args, enum state *);
-void quit(int nargs, char ** args, enum state *);
-void send_text(int nargs, char ** args, enum state *);
+void receive_loop(sock_t sockfd, enum state *); // this needs to be activated in session 
+int send_n_receive(sock_t sockfd, char * finalPacket, char * message, enum state *);
+void construct_packet_client(struct Message, packet_t type, char * username, char * finalPacket);
+sock_t login(int nargs, char ** args, sock_t, enum state *);
+void logout(int nargs, char ** args, sock_t, enum state *);
+void join_session(int nargs, char ** args, sock_t, enum state *);
+void leave_session(int nargs, char ** args, sock_t, enum state *);
+void create_session(int nargs, char ** args, sock_t, enum state *);
+void list(int nargs, char ** args, sock_t, enum state *);
+void quit(int nargs, char ** args, sock_t, enum state *);
+void send_text(char * text, sock_t, enum state *);
 void print_menu();
+void print_short_menu();
 
 
 
--- part1/include/constants.h
+++ part1/include/constants.h
@@ -4,9 +4,11 @@
 #define INPUT_LENGTH 128
 #define MAXBUFLEN 1000
 #define STR(x) #x
+#define USERNAME_SET NULL
 #define ___space___(x) ("\n"#x"\n")
 
 typedef int sock_t;
 typedef int packet_t;
+typedef int error_t;
 
 #endif
